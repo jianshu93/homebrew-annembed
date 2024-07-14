5af338b7994e14f8023f7126e6a8b9879352bca3
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-07-14 14:52:04 -0400

    Update annembed.rb

--- Formula/annembed.rb
+++ Formula/annembed.rb
@@ -4,9 +4,9 @@
 class Annembed < Formula
   desc "Data embedding based on graph-based approximate nearest neighbour"
   homepage "https://github.com/jean-pierreBoth/annembed"
-  url "https://github.com/jianshu93/annembed/releases/download/v0.1.2/annembed_universal_v0.1.2.tar.gz"
-  sha256 "433cafd095dc24f4aeaff10aadfe1f7f2f1af43c700e1ea8e89d156b4bf844dd"
-  version "0.1.2"
+  url "https://github.com/jianshu93/annembed/releases/download/v0.1.4/annembed_universal_v0.1.4.tar.gz"
+  sha256 "310e0028ef9985b4d18072d2a718d407f21c6b9444eb69b6de84ab64107ed5f7"
+  version "0.1.4"
 
   def install
     bin.install "annembed"
