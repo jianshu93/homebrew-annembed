e84be08dd8b2a478cb8229472ca039acbb665c42
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2024-11-08 16:12:47 -0800

    Update annembed to v0.1.8
    update to v0.1.8

--- Formula/annembed.rb
+++ Formula/annembed.rb
@@ -4,9 +4,9 @@
 class Annembed < Formula
   desc "Data embedding based on graph-based approximate nearest neighbour"
   homepage "https://github.com/jean-pierreBoth/annembed"
-  url "https://github.com/jianshu93/annembed/releases/download/v0.1.6/annembed_universal.tar.gz"
+  url "https://github.com/jianshu93/annembed/releases/download/v0.1.7/annembed_universal.tar.gz"
   sha256 "d5f7a43dba2221629498fcaf7ef8a1310f16870f1556a37804ae41f99fadbc71"
-  version "0.1.7"
+  version "0.1.8"
 
   def install
     bin.install "annembed"
