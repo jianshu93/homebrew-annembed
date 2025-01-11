f42f7bf4b3418754cb61051b8e1b30855a818031
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-01-11 12:44:45 -0800

    Update annembed to v0.1.8
    add HubNSW

--- Formula/annembed.rb
+++ Formula/annembed.rb
@@ -4,9 +4,9 @@
 class Annembed < Formula
   desc "Data embedding based on graph-based approximate nearest neighbour"
   homepage "https://github.com/jean-pierreBoth/annembed"
-  url "https://github.com/jianshu93/annembed/releases/download/v0.1.7/annembed_universal.tar.gz"
-  sha256 "d5f7a43dba2221629498fcaf7ef8a1310f16870f1556a37804ae41f99fadbc71"
-  version "0.1.8"
+  url "https://github.com/jianshu93/annembed/releases/download/v0.1.8/annembed_Darwin_universal_v0.1.8.tar.gz"
+  sha256 "bdbe88e81a7982d5b33dbdf93ac343acdde46d2403a59f4aea8f6feaeb4a7822"
+  version "0.1.9"
 
   def install
     bin.install "annembed"
