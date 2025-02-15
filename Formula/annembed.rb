3a982e6b4908af07dab887eac6ac62dfeca17301
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2025-02-15 00:43:07 -0800

    Update annembed to 0.2.1
    better diffusion map in v0.2.1

--- Formula/annembed.rb
+++ Formula/annembed.rb
@@ -3,10 +3,10 @@
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Annembed < Formula
   desc "Data embedding based on graph-based approximate nearest neighbour"
-  homepage "https://github.com/jean-pierreBoth/annembed"
-  url "https://github.com/jianshu93/annembed/releases/download/v0.1.9/annembed_universal.tar.gz"
-  sha256 "da548f7b81046f5a32d2f962c9c56fb75c85948e7cdab5634b0aa824ac2f35af"
-  version "0.2.0"
+  homepage "https://github.com/jianshu93/annembed"
+  url "https://github.com/jianshu93/annembed/releases/download/v0.2.0/annembed_universal.tar.gz"
+  sha256 "bf03462c8a2bbf2cda5380e6efdd32c48486d985b72c3e757453f05c461726bf"
+  version "0.2.1"
 
   def install
     bin.install "annembed"
